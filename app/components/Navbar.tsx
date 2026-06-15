cedc26ee8c7b44b3362d217d3c0952c787129292
6821901030-png <6821901030@cdti.ac.th>
2026-06-15 10:23:12 +0700

    first commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbsr(){
+    return(
+        <div>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about"> About <a></li>
+
+            </ul>
+        </div>
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Navbsr from "./components/Navbar";
+
+export default function Home() {
+  return (
+    <div >
+      <h1>welcome at NExt.js</h1>
+      <p>suphawat</p>
+    </div>
+  );
+}
