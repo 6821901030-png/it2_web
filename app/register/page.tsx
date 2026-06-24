64cee00a9a924b9d42d3ee863a1606caca788b90
6821901030-png <6821901030@cdti.ac.th>
2026-06-24 10:14:53 +0700

    updata

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,13 +1,48 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 export default function Register() {
-    return(
+    const router = useRouter();
+
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
+
+    async function handleSubmit(e: React.FormEvent) {
+        e.preventDefault();
+
+        const res = await fetch("/api/auth/register", {
+            method: "POST",
+            body: JSON.stringify(form),
+        });
+
+        if (res.ok) {
+            router.push("/login");
+        } else {
+            alert("Register failed");
+        }
+    }
+    return (
         <div className="auth-page">
-            <form className="auth-card">
+            <form className="auth-card" onSubmit={handleSubmit}>
                 <h1> สมัครสมาชิก </h1>
-                <input placeholder="ชื่อ นามสกุล" />
-                <input placeholder= "Email" />
-                <input placeholder= "Password" />
+                <input
+                    placeholder="ชื่อ นามสกุล"
+                    onChange={(e) => setForm({ ...form, name: e.target.value })}
+                />
+                <input
+                    placeholder="Email"
+                    onChange={(e) => setForm({ ...form, email: e.target.value })}
+                />
+                <input
+                    placeholder="Password"
+                    onChange={(e) => setForm({ ...form, password: e.target.value })}
+                />
                 <button> Register </button>
             </form>
         </div>
     );
-  }
+}
