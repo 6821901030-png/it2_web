012a57f299dc1a296a8ee745840e900c7857c0e4
6821901030-png <6821901030@cdti.ac.th>
2026-07-15 10:26:00 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
-import Navbsr from "./components/Navbar";
 
 export default function Home() {
   return (
-    <div >
-      <h1>welcome at NExt.js</h1>
-      <p>suphawat</p>
+    <div className="container">
+      <h1>ต้อนรับสู่แอพของฉัน</h1>
+     
     </div>
   );
 }
